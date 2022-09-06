3f86bef2341e1141e5532f1910f6e29b1374fbcb
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-09-06 13:33:23 -0300

    Inicio page resenha (não finalizada)

--- src/views/IndividualAskon.vue
+++ src/views/IndividualAskon.vue
@@ -29,3 +29,40 @@ export default {
 </script>
 
 <style></style> -->
+
+<template>
+  <div class="tudo">
+    <div class="container-resenha-individual">
+      <div class="titulo-resenha">Titulo da Resenha :)
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.tudo {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-color: red;
+}
+.container-resenha-individual {
+    width: 50%;
+    height: 1500px;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    margin: 250px auto;
+    background-color: #111;
+}
+</style>
