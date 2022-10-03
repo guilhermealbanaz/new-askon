2c54e18e94a44b69e0bf656d1ec479cb95c9d667
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-10-03 00:20:25 -0300

    carrossel

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueCarousel from 'vue-carousel';
 import Vue2Filters from 'vue2-filters';
 import App from "./App.vue";
 import router from "./router";
@@ -9,6 +10,7 @@ import VueRellax from 'vue-rellax';
 Vue.config.productionTip = false;
 Vue.use(VueRellax)
 Vue.use(Vue2Filters)
+Vue.use(VueCarousel);
 
 new Vue({
   router,
--- src/views/HomeAskon.vue
+++ src/views/HomeAskon.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="tudo">
+    <carousel navigationPrevLabel="" navigationNextLabel="" ref="carousel" :perPage="Number(1)" :autoplayTimeout="Number(2000)" :navigationEnabled="true" :navigationClickTargetSize="Number(20)" paginationActiveColor="#fff" :paginationSize="35" paginationColor="#020013" :autoplay="true" :loop="true">
+        <slide v-for="(imagem,index) in imagensTopicos" :key="index">
+          <img :src="imagem" alt="" />
+        </slide>
+    </carousel>
     <BodyAskon></BodyAskon>
     <!-- <div class="container-resenha">
       <div >
@@ -138,19 +143,41 @@
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel';
 // import mdiStar from "vue-material-design-icons/Star.vue";
 // import mdiGithub  from "vue-material-design-icons/Github.vue";
 import BodyAskon from '@/components/BodyAskon.vue'
 export default {
   components: {
     BodyAskon,
+    Carousel,
+    Slide
+  
     // mdiStar,
     // mdiGithub, 
     // mdiLinkedin,
   },
+  data(){
+    return {
+      imagensTopicos: [require('@/assets/akali.jpg'), require('@/assets/akali2.jpg'), require('@/assets/akali3.jpg')] 
+    }
+  }
 };
 </script>
 <style>
+.carrossel{
+  position: relative;
+  height: 700px;
+}
+.VueCarousel{
+  height: 700px;
+}
+.VueCarousel img {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  margin: 10px
+}
 
 
 </style>
