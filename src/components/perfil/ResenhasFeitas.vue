5b659a401ed81f71758d8c1f23ffc8558ebd2d2d
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-10-30 16:43:39 -0300

    resenhas em si

--- src/components/perfil/ResenhasFeitas.vue
+++ src/components/perfil/ResenhasFeitas.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container-resenhas-perfil">
-        <div class="box-resenhas-perfil" v-for="resenha in resenhas" :key="resenha.id">
+        <!-- <div class="box-resenhas-perfil" v-for="resenha in resenhas" :key="resenha.id">
           <div class="img-resenha-perfil"></div>
           <div class="box-texto-resenha-perfil">
             <div class="titulo-perfil">{{resenha.titulo}}</div>
@@ -11,18 +11,31 @@
             
           </div>
           </div>
-        </div>
+        </div> -->
+        <ResenhasAskon
+        id="perfil"
+        v-for="resenha in resenhas"
+        :key="resenha.id"
+        :ImgName="'akali2.jpg'"
+        :ResenhaTitles="resenha.titulo"
+        :ResenhaDate="resenha.data"
+        :Estrela="resenha.estrela"
+        />
         </div>
 </template>
 
 <script>
+import ResenhasAskon from '@/components/ResenhasAskon.vue'
 import { mapState } from "vuex";
 import axios from "axios";
 export default {
+  components:{
+        ResenhasAskon
+    },
   computed: {
       ...mapState("auth", ["loggedIn"]),
       ...mapState('usuariovisitado', ['usuariovisitado'])
-},
+    },
     data(){
         return{
             resenhas: [],
